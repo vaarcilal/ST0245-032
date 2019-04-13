1d6b59fc30f2a9ddb8af195033a5ff7d58043e5a
Vincent A. Arcila L <vaarcilal@eafit.edu.co>
2019-04-12 19:12:31 -0500

    No descargar!

--- proyecto/codigo/DATASET/convertirDatasets.py
+++ proyecto/codigo/DATASET/convertirDatasets.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Author: Vincent A. Arcila L.
 Email: vaarcilal@eafit.edu.co
@@ -11,10 +12,11 @@ import math
 r = 6378100 # earth's radius
 
 fileToRead = open(argv[1],"r")
-fileToWrite = open(argv[1].strip(".txt") + "EnMetros.txt", "w") # file on which translations will be put
+fileToWrite = open(argv[1].strip(".txt") + "EnMetros.csv", "w") # file on which translations will be put
 
 counter = 0;
 origin = []
+fileToWrite.write("x,y,z\n")
 for line in fileToRead.readlines()[1:]:
     if counter == 0:
         line.strip("\n") # strip \n at the end of the line
@@ -31,7 +33,7 @@ for line in fileToRead.readlines()[1:]:
         counter2 += 1
     counter2 = 0
 
-    coordinates = [(2 * math.pi * r * float(i))/360 for i in coordinates] # translate: 2 * pi * radius * angle / 360
+    coordinates = [(2 * math.pi * r * float(i))/360 for i in coordinates[:2]] + [coordinates[2]] # translate: 2 * pi * radius * angle / 360
     fileToWrite.write( str(coordinates[0]) + ",") 
     fileToWrite.write( str(coordinates[1]) + ",") 
     fileToWrite.write( str(coordinates[2]) + "\n") 
